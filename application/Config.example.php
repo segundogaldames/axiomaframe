b7ec780d24bc450625a647658f0ee02b8c1d1f09
Segundo <segundogaldames@gmail.com>
2022-07-09 11:03:37 -0400

    Archivo de configuraciones de acceso

---
+++ application/Config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+define('BASE_URL', 'RUTA WEB DEL PROYECTO');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('SESSION_TIME', 10);
+define('HASH_KEY', '62c984b6ae3fe62c984cfc4e8c');
+define('CTRL', '62c984e77ad1062c984f045a28');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_NAME', 'base');
+define('DB_CHAR', 'utf8');
+define('METHODENCRIPT','AES-256-ECB');
+define('KEY','APP_AXIOMADEV');
