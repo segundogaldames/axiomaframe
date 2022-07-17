452f98c4e44706693b58c0a01cc90cd258f06d22
Segundo <segundogaldames@gmail.com>
2022-07-17 17:57:43 -0400

    Constante driver para config.example

--- application/Config.example.php
+++ application/Config.example.php
@@ -9,6 +9,7 @@ define('SESSION_TIME', 10);
 define('HASH_KEY', '62c984b6ae3fe62c984cfc4e8c');
 define('CTRL', '62c984e77ad1062c984f045a28');
 
+define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'user');
 define('DB_PASS', 'password');
